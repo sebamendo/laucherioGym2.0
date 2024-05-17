00f702263ed946b5d48382090df71c00cb0a01b4
sebamendo <109764839+sebamendo@users.noreply.github.com>
2024-05-17 01:57:28 -0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    $("#lunes-jueves").on("touchstart", function(event) {
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event) {
+            var xMove = event.originalEvent.touches[0].pageX;
+            if (Math.floor(xClick - xMove) > 5) {
+                $(this).carousel('next');
+            } else if (Math.floor(xClick - xMove) < -5) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on("touchend", function() {
+            $(this).off("touchmove");
+        });
+    });
+});
+
+$(document).ready(function() {
+    $("#martes-viernes").on("touchstart", function(event) {
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event) {
+            var xMove = event.originalEvent.touches[0].pageX;
+            if (Math.floor(xClick - xMove) > 5) {
+                $(this).carousel('next');
+            } else if (Math.floor(xClick - xMove) < -5) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on("touchend", function() {
+            $(this).off("touchmove");
+        });
+    });
+});
+
+$(document).ready(function() {
+    $("#miercoles-sabados").on("touchstart", function(event) {
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event) {
+            var xMove = event.originalEvent.touches[0].pageX;
+            if (Math.floor(xClick - xMove) > 5) {
+                $(this).carousel('next');
+            } else if (Math.floor(xClick - xMove) < -5) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on("touchend", function() {
+            $(this).off("touchmove");
+        });
+    });
+});
+
+$(document).ready(function() {
+    $("#lunes").on("touchstart", function(event) {
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event) {
+            var xMove = event.originalEvent.touches[0].pageX;
+            if (Math.floor(xClick - xMove) > 5) {
+                $(this).carousel('next');
+            } else if (Math.floor(xClick - xMove) < -5) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on("touchend", function() {
+            $(this).off("touchmove");
+        });
+    });
+});
+
+$(document).ready(function() {
+    $("#miercoles").on("touchstart", function(event) {
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event) {
+            var xMove = event.originalEvent.touches[0].pageX;
+            if (Math.floor(xClick - xMove) > 5) {
+                $(this).carousel('next');
+            } else if (Math.floor(xClick - xMove) < -5) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on("touchend", function() {
+            $(this).off("touchmove");
+        });
+    });
+});
+
+$(document).ready(function() {
+    $("#viernes").on("touchstart", function(event) {
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event) {
+            var xMove = event.originalEvent.touches[0].pageX;
+            if (Math.floor(xClick - xMove) > 5) {
+                $(this).carousel('next');
+            } else if (Math.floor(xClick - xMove) < -5) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on("touchend", function() {
+            $(this).off("touchmove");
+        });
+    });
+});
